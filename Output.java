1ff9b00eb76e25b76eb1f3af558b7598bbe8c0ad
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2024-10-07 20:53:08 +0530

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,9 @@
+public class Output {
+  public static void main(String[] args) {
+    //output
+    System.out.print("Nikhilesh Tiwari,java is very intresting");
+    System.out.println("Hello");
+    System.out.println("Hello EveryOne");
+    System.out.print("To print the output in next line in print function we use \n");
+  }  
+}
