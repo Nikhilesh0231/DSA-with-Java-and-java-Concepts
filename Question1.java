7d301ba7c51f80fe780fd571d9dd4daa701fd26f
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2024-10-07 20:53:24 +0530

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,14 @@
+
+public class Question1 {
+
+/*Print the pattern using print fuction
+*
+**
+***
+****  
+*/
+    public static void main(String[] args) {
+        System.out.print("*\n**\n***\n****");
+
+    }
+}
