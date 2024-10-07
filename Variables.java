3f0c3113c7534add9b2d49bc03e16d4d6e75653b
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2024-10-07 20:54:39 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+  public static void main(String[] args) {
+    //Variables
+    int x = 10; // integer
+    double y = 20.5; // double
+    boolean z = true; // boolean
+    String name = "Nikhilesh";//String 
+    //Variables can be updated or we can say that they are mutable.
+    name = "Tiwari";
+    x= 20;
+    int a = 20;
+    int b = 10;
+    int sum = a+b;
+    System.out.println("Sum is :"+sum);
+    int diff = a-b;
+    System.out.println("diff is :"+diff);
+    int mul = a*b;
+    System.out.println("mul is : "+mul);
+  }  
+}
