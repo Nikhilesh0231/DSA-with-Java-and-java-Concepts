33a83c860e458600661d78612b6579dca7e333d5
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2024-10-09 23:39:35 +0530

    Add files via upload

---
+++ ProductTwoNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ProductTwoNumbers {
+  public static int productNum(int num1,int num2){
+    int product ;
+    product = num1 * num2 ;
+    return product ;
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the first number: ");
+    int num1 = sc.nextInt();
+    System.out.print("Enter the second number: ");
+    int num2 = sc.nextInt();
+    int product = productNum(num1,num2);
+    System.out.println("The product of the two numbers is: "+product);
+  }
+}
