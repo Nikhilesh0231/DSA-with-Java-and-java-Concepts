93092f7f7ed11a48a682e76cdb3c7e212fb2b508
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2024-10-10 01:39:12 +0530

    Add files via upload

---
+++ Question9.java
@@ -0,0 +1,10 @@
+public class Question9 {
+  //Question--> Write a infinite loop using do while condition.
+  public static void main(String[] args) {
+    int i = 0;
+    do { 
+      System.out.println("Hello I am infinite loop!!!");
+      i++;
+    } while (i>0);
+  }
+}
