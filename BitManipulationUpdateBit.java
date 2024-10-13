335f20354786fe86a894e5cc099c0bda5e3042ef
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2024-10-13 09:32:59 +0530

    Add files via upload

---
+++ BitManipulationUpdateBit.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class BitManipulationUpdateBit {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int oper = sc.nextInt();
+    //oper = 1 : set         oper = 0 : clear
+    int number = 5;//0101
+    int pos = 1;
+    
+    int bitMask = 1 << pos;
+    if (oper == 1) {
+      //set
+      int newnumber = number | bitMask;
+      System.out.println(newnumber);
+      } else if (oper == 0) {
+        //clear
+      int newnumber = number & ~bitMask;
+      System.out.println(newnumber);
+
+      }
+  }
+}
